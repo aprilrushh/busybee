05830875c44bd347af60fd28c1704a30e76f328e
aprilrushh <aprilrushh@gmail.com>
2025-02-13 18:18:24 +0900

    Fix syntax error and simplify background pattern

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ const App = () => {
       {/* Hero Section */}
       <header className="relative bg-gradient-to-r from-red-600 to-red-800 text-white overflow-hidden">
         <div className="absolute inset-0">
-          <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"30\" height=\"30\" viewBox=\"0 0 30 30\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cpath d=\"M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z\" fill=\"rgba(255,255,255,0.07)\"%3E%3C/path%3E%3C/svg%3E')] opacity-20"></div>
+          <div className="absolute inset-0 opacity-20 bg-pattern"></div>
         </div>
         <div className="relative">
           <div className="container mx-auto px-6 py-24">
